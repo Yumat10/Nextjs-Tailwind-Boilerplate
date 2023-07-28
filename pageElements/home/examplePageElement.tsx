f60eb096563fe80b8249475b82b91cb475a25f19
Yuma Tanaka <yumatanaka@Yumas-MacBook-Pro.local>
2023-07-28 08:43:24 -0700

    Added metatag, loading spinner, and example page element components

---
+++ pageElements/home/examplePageElement.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+type examplePageElementProps = {};
+
+export const examplePageElement: FC<examplePageElementProps> = ({}) => {
+  return <div>Example Page Element</div>;
+};
