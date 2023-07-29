75b218dd880c0946454a83edebcf7aa74074e777
Yuma Tanaka <yumatanaka@Yumas-MacBook-Pro.local>
2023-07-28 17:46:09 -0700

    Updated metatag title

--- components/metaTags/MetaTags.tsx
+++ components/metaTags/MetaTags.tsx
@@ -31,7 +31,7 @@ export const MetaTags: FC<MetaTagsProps> = ({ subtitle }) => {
 
       <meta property="twitter:card" content="summary_large_image" />
       <meta property="twitter:url" content={url} />
-      <meta property="twitter:title" content="Chameleon" />
+      <meta property="twitter:title" content={title} />
       <meta property="twitter:description" content={description} />
       <meta property="twitter:image" content={imageUrl} />
 
